c2a4529039b8874151abd90f8422893520604afd
Joshua Strobl <joshua@stroblindustries.com>
2017-07-23 17:48:29 +0300

    Change to using GTK3_MODULES so it doesn't attempt to use it for GTK2 apps as well.

--- files/70-plotinus.sh
+++ files/70-plotinus.sh
@@ -1,9 +1,9 @@
 # Begin /usr/share/defaults/etc/profile.d/70-plotinus.sh
 
-if [ -z "${GTK_MODULES}" ]; then
-    export GTK_MODULES="/usr/lib/libplotinus.so"
+if [ -z "${GTK3_MODULES}" ]; then
+    export GTK3_MODULES="/usr/lib/libplotinus.so"
 else
-    export GTK_MODULES="$GTK_MODULES:/usr/lib/libplotinus.so"
+    export GTK3_MODULES="$GTK3_MODULES:/usr/lib/libplotinus.so"
 fi
 
 # End /usr/share/defaults/etc/profile.d/70-plotinus.sh
